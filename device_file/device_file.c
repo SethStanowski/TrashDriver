724083fc2d9bb261f4e33ccff53148b0a1088d32
sethstanowski <sethcstanowski@gmail.com>
2021-04-13 21:27:25 -0400

    embetronicx device file lession 5 part 2
    finished lession 5

--- device_file/device_file.c
+++ device_file/device_file.c
@@ -3,8 +3,16 @@
 #include<linux/module.h>
 #include<linux/kdev_t.h>
 #include<linux/fs.h>
+#include<linux/device.h>
 
 dev_t dev =0;
+static struct class *dev_class;
+
+
+/*
+ *  Module init function
+ */
+
 
 static int __init hello_world_init(void) {
 	/*Allocating Major number */
@@ -13,11 +21,38 @@ static int __init hello_world_init(void) {
 		return -1;
 	}
 	printk(KERN_INFO "Major = %d Minor = %d \n", MAJOR(dev), MINOR(dev));
+
+	/*Creating struct class */
+	if ((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL) {
+		printk(KERN_INFO "Cannot create the struct class for device\n");
+		goto r_class;
+	}
+
+	/*Creating device*/
+	if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL) {
+		printk(KERN_INFO "Cannot create the Device\n");
+		goto r_device;
+	}
+
 	printk(KERN_INFO "Kernel Module Inserted Successfully....\n");
 	return 0;
+
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev,1);
+	return -1;
+
 }
 
+
+/*
+ *  Module exit function
+*/
+
 void __exit hello_world_exit(void) {
+	device_destroy(dev_class,dev);
+	class_destroy(dev_class);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "Kernel Module Removed Successfully....\n");
 }
