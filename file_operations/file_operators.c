b20e97c623140021f1408a1283f7fa10ddc6229f
sethstanowski <sethcstanowski@gmail.com>
2021-04-15 20:18:27 -0400

    driver opererations embetronicx Tutorial 6
    finished tutorial 6

--- file_operations/file_operators.c
+++ file_operations/file_operators.c
@@ -70,7 +70,7 @@ static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t 
  */
 
 static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
-	printk(KERN_NFO "Deriver write function called....!!!");
+	printk(KERN_INFO "Deriver write function called....!!!");
 	return len;
 }
 
@@ -78,4 +78,60 @@ static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, 
  * Module init function
  */
 
+static int __init etx_driver_init(void) {
+	/* Allocating Major number*/
+	if((alloc_chrdev_region(&dev, 0,1, "etx_Dev")) <0) {
+		printk(KERN_INFO "Cannot allocate major number \n");
+		return -1;
+	}
+	printk(KERN_INFO "Major =%d Minor = %d \n", MAJOR(dev), MINOR(dev));
 
+	/*Creating cdev structure */
+	cdev_init(&etx_cdev,&fops);
+
+	/* Adding character device to the system */
+	if((cdev_add(&etx_cdev,dev,1)) < 0) {
+		printk(KERN_INFO "Cannot add the device to the system\n");
+		goto r_class;
+	}
+
+	/*Creating struct class */
+	if((dev_class = class_create(THIS_MODULE, "etx_class")) == NULL) {
+		printk(KERN_INFO "Cannot create the struct class\n");
+		goto r_class;
+	}
+
+	/*Creating device */
+	if((device_create(dev_class, NULL,dev,NULL, "etx_device")) == NULL ) {
+		printk(KERN_INFO "Cannot create the Device 1\n");
+		goto r_device;
+	}
+	printk(KERN_INFO "Device Driver Insert...Done!!!\n");
+	return 0;
+
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev,1);
+	return -1;
+}
+
+
+/*
+ *  Module exit function
+*/
+static void __exit etx_driver_exit(void) {
+	device_destroy(dev_class,dev);
+	class_destroy(dev_class);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev,1);
+	printk(KERN_INFO "Device Driver Removed...Done!!!\n");
+}
+
+
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("Simple Linux device driver (File Operations)");
+MODULE_VERSION("1.3");
