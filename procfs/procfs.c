03f882985ed05275beabeadd657c4a0e7a4723b7
sethstanowski <sethcstanowski@gmail.com>
2021-04-22 21:26:43 -0400

    finished lession 7 procfs embetronicx tutorial
    finished lession 7

--- procfs/procfs.c
+++ procfs/procfs.c
@@ -48,7 +48,7 @@ static int 	__init etx_driver_init(void);
 static void 	__exit etx_driver_exit(void);
 
 /*************** Driver Functions **********************/
-static int 	etx_open(struct inode *inode, struct file *file);
+static int      etx_open(struct inode *inode, struct file *file);
 static int 	etx_release(struct inode *indoe, struct file *file);
 static ssize_t  etx_read(struct file *filp, char __user *buf, size_t len, loff_t * off);
 static ssize_t	etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
@@ -57,7 +57,7 @@ static long 	etx_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 /***************** Procfs Functions *******************/
 static int 	open_proc(struct inode *inode, struct file *file);
 static int 	release_proc(struct inode *inode, struct file *file);
-static ssize_t  read_proc(struct file *filp, char __user *buffer, size_t length, loff * offset);
+static ssize_t  read_proc(struct file *filp, char __user *buffer, size_t length, loff_t * offset);
 static ssize_t  write_proc(struct file *filp, const char *buff, size_t len, loff_t * off);
 
 /*
@@ -67,8 +67,8 @@ static struct file_operations fops =
 {
 	.owner 		= THIS_MODULE,
 	.read 		= etx_read,
-	.write 		= etx_open,
-	.open 		= etx_ioctl,
+	.write 		= etx_write,
+	.open		= etx_open,
 	.unlocked_ioctl	= etx_ioctl,
 	.release 	= etx_release,
 };
@@ -98,7 +98,7 @@ static struct file_operations proc_fops = {
 /*
  * This functino will be called when we open the profcs file
  */
-static int open_proc(struct inode *indoe, struct file *file) {
+static int open_proc(struct inode *inode, struct file *file) {
 	pr_info("proc file opened.....\t");
 	return 0;
 }
@@ -106,7 +106,7 @@ static int open_proc(struct inode *indoe, struct file *file) {
 /*
  * this function will be called when we close the procfs file
  */
-static int release_proc(struct indoe *inode, struct file *file) {
+static int release_proc(struct inode *inode, struct file *file) {
 	pr_info("proc file release....\n");
 	return 0;
 }
@@ -129,4 +129,146 @@ static ssize_t read_proc(struct file *filp, char __user *buffer, size_t length, 
 
 	return length;;
 }
+
+/*
+ * This function will be called when we write the procfs file
+ */
+static ssize_t write_proc(struct file *filp, const char *buff, size_t len, loff_t * off) {
+	pr_info("proc file wrote....\n");
+
+	if( copy_from_user(etx_array,buff,len) ) {
+		pr_err("Data Write : Err!\n");
+	}
+
+	return len;
+}
+
+/*
+** This function will be called when we open the Device file
+*/
+static int etx_open(struct inode *inode, struct file *file)
+{
+        pr_info("Device File Opened...!!!\n");
+        return 0;
+}
+
+
+/*
+ * This function will be called when we close the Device file
+ */
+static int etx_release(struct inode *inode, struct file *file) {
+	pr_info("Device File Closed...!!!\n");
+	return 0;
+}
+
+/*
+ * This function will be called when we read the Device file
+ */
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t * off) {
+	pr_info("Read function\n");
+	return 0;
+}
+
+/*
+ * This function will be called when we write teh Device file
+ */
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t * off) {
+	pr_info("Write Function\n");
+	return len;
+}
+
+/*
+ * This function will be called when we write IOCTL on the Device file
+ */
+static long etx_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+	switch(cmd) {
+		case WR_VALUE:
+			if( copy_from_user(&value ,(int32_t*) arg, sizeof(value)) ) {
+				pr_err("Data Write : Err!\n");
+			}
+			pr_info("Value = %d\n", value);
+			break;
+		case RD_VALUE:
+			if( copy_to_user((int32_t*) arg, &value, sizeof(value)) ) {
+				pr_err("Data Read : Err!\n");
+			}
+			break;
+		default:
+			pr_info("Default\n");
+			break;
+	}
+	return 0;
+}
+
+/*
+ * Module Init function
+ */
+static int __init etx_driver_init(void) {
+	/*Allocating Major number */
+	if(( alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) < 0) {
+		pr_info("Cannot allocate major number \n");
+		return -1;
+	}
+	pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+	/*Creating cdev structure*/
+	cdev_init(&etx_cdev,&fops);
+
+	/*Adding character device to the system*/
+	if((cdev_add(&etx_cdev,dev,1)) < 0) {
+		pr_info("Cannot create teh struct class\n");
+		goto r_class;
+	}
+
+	/*Creating device*/
+	if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL) {
+		pr_info("Cannot create the Device 1\n");
+		goto r_device;
+	}
+
+	/*Create proc dicectory. It will create a directory under "/proc" */
+	parent = proc_mkdir("etx",NULL);
+
+	if( parent == NULL) {
+		pr_info("Error creating proc entry");
+		goto r_device;
+	}
+
+	/*Creating Proc entry under "/proc/etc/" */
+	proc_create("etx_proc", 0666, parent, &proc_fops);
+
+	pr_info("Device Driver Insert...Done!!\n");
+	return 0;
+
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev,1);
+	return -1;
+}
+
+/*
+ * Module exit function
+ */
+static void __exit etx_driver_exit(void) {
+	/* Removes singe proc entry */
+	//remove_proc_entry("etx/etx_proc", parent);
+        
+        /* remove complete /proc/etx */
+        proc_remove(parent);
+        
+        device_destroy(dev_class,dev);
+        class_destroy(dev_class);
+        cdev_del(&etx_cdev);
+        unregister_chrdev_region(dev, 1);
+        pr_info("Device Driver Remove...Done!!!\n");
+}
+ 
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("Simple Linux device driver (procfs)");
+MODULE_VERSION("1.6");
 
