fc704433d7aa4c9bb36d933750176f820a8b0d14
sethstanowski <sethcstanowski@gmail.com>
2021-04-28 21:51:30 -0400

    started dynamic queue
    started dynamic queue

---
+++ waitqueue/dynamicwait.c
@@ -0,0 +1,21 @@
+/****************************************************************************//**
+*  \file       driver.c
+*
+*  \details    Simple linux driver (Waitqueue Dynamic method)
+*
+*  \author     EmbeTronicX
+*
+*  \Tested with Linux raspberrypi 5.10.27-v7l-embetronicx-custom+
+*
+*******************************************************************************/
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/slab.h>                 //kmalloc()
+#include <linux/uaccess.h>              //copy_to/from_user()
+#include <linux/kthread.h>
+#include <linux/wait.h>                 // Required for the wait queues
