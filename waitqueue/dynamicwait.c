075cb3ed714541f784690e8786eec7e65e08739d
sethstanowski <sethcstanowski@gmail.com>
2021-05-04 20:41:39 -0400

    chapter 10 progress
    chapter 10 progress

--- waitqueue/dynamicwait.c
+++ waitqueue/dynamicwait.c
@@ -110,4 +110,72 @@ static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, 
  * Module Init function
  */
 
+static int __init etx_driver_init(void) {
+	/*Allocating Major number*/
+	if((alloc_chrdev_region(&dev, 0 ,1, "etx_Dev")) < 0 ) {
+		pr_info("Cannot allocate major number\n");
+		return -1;
+	}
+	pr_info("Major = %d Minor = %d \n", MAJOR(dev), MINOR(dev));
 
+	/*creating cdev structure*/
+	cdev_init(&etx_cdev,&fops);
+
+	/*Adding character device to the system*/
+	if((cdev_add(&etx_cdev,dev,1)) <0){
+		pr_info("Cannot add the device to the system\n");
+		goto r_class;
+	}
+
+	/*Creating struct class*/
+	if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL ) {
+		pr_info("Cannot create teh struct class\n");
+		goto r_class;
+	}
+
+	/*createing device*/
+	if((device_create(dev_class, NULL, dev,NULL, "etx_device")) == NULL) {
+		pr_info("Cannot create teh Device 1\n");
+		goto r_device;
+	}
+
+	//Initializing wait queue
+	init_waitqueue_head(&wait_queue_etx);
+
+	//Create the kernel thread with the name 'mythread'
+	wait_thread = kthread_create(wait_function, NULL, "WaitThread");
+	if (wait_thread) {
+		pr_info("Thread Created successfully\n");
+		wake_up_proccess(wait_thread);
+	}
+	else
+		pr_info("Thread creation failed\n");
+	pr_info("Device Driver Insert...Done!!!\n");
+	return 0;
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev,1);
+	return -1;
+}
+
+/*
+ * Module exit function
+ */
+static void __exit etx_driver_exit(void){
+	wait_queue_flag = 2;
+	wake_up_interruptible(&wait_queue_etx);
+	device_destroy(dev_class,dev);
+	class_destroy(dev_class);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev,1 );
+	pr_info("Device Driver Remove...Done!!!\n");
+}
+
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("Simple linux driver (Waitqueue Dynamic method)");
+MODULE_VERSION("1.8");
